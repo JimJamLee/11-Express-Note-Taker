4b761c0d9f77b9a7a7158ae1c5703b96a3c86fdb
JimJamLee <li.james.chien@gmail.com>
2022-09-20 23:32:05 -0700

    Updated routes.js with the delete function

--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,23 @@
 const notes = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
+const path = require('path');
 const { readNote, writeNote, editNote } = require('../helpers/fsUtils');
-const notesData = require('../db/db.json');
+const notesData = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "../db/db.json"), (err, data) => {
+      if (err) throw err;
+    })
+  );
+
+function writeNotesData(notesData) {
+    fs.writeFileSync(
+      path.join(__dirname, "../db/db.json"),
+      JSON.stringify(notesData),
+      err => {
+        if (err) throw err;
+      }
+    );
+  }
 
 notes.get('/', (req, res) =>
   readNote('./db/db.json').then((data) => res.json(JSON.parse(data)))
@@ -26,5 +41,17 @@ notes.post('/', (req, res) => {
 
     res.json(response);
 });
+
+notes.delete("/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    for (let i = 0; i < notesData.length; ++i) {
+      if (id === notesData[i].id) {
+        notesData.splice(i, 1);
+        return;
+      }
+    }
+    writeNotesData(notesData);
+    res.send(notesData);
+  });
 
 module.exports = notes;
